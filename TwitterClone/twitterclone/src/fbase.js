341b744501a80c230eb9f90df78a4e9e8268ffd0
antifly55 <antifly55@kookmin.ac.kr>
2021-02-09 11:54:58 +0900

    TwitterClone: add simple CRUD

--- TwitterClone/twitterclone/src/fbase.js
+++ TwitterClone/twitterclone/src/fbase.js
@@ -1,7 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 import firebaseConfig from "./env";
 
 firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- TwitterClone/twitterclone/src/routes/Home.js
+++ TwitterClone/twitterclone/src/routes/Home.js
@@ -1,4 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { dbService } from "../fbase";
+import Tweet from "../components/Tweet";
 
-const Home = () => <span>Home</span>;
+const Home = ({userObj}) => {
+    const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
+    useEffect(() => {
+        dbService.collection("tweets").onSnapshot((snapshot) => {
+            const tweetArray = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            setTweets(tweetArray);
+          });
+    }, []);
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      await dbService.collection("tweets").add({
+          text: tweet,
+          createdAt: Date.now(),
+          creatorId: userObj.uid,
+      });
+      setTweet("");
+    };
+    const onChange = (event) => {
+      const {
+        target: { value },
+      } = event;
+      setTweet(value);
+    };
+    return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            value={tweet}
+            onChange={onChange}
+            type="text"
+            placeholder="What's on your mind?"
+            maxLength={120}
+          />
+          <input type="submit" value="Tweet" />
+        </form>
+        <div>
+            {tweets.map((tweet) => (
+                <Tweet
+                    key={tweet.id}
+                    tweetObj={tweet}
+                    isOwner={tweet.creatorId === userObj.uid}
+                />
+            ))}
+        </div>
+      </div>
+    );
+};
 export default Home;
